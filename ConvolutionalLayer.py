8dde00168ca78972d8dc4a4d5b2950ef0a390f33
mpslxz <seyedmehran.pesteie@gmail.com>
2016-01-27 15:20:45 -0800

    Deep neural network V.1.0 includes:
    1. Convolutional-pooling layer
    2. Dropout layer
    3. Fully-connected layer
    4. Softmax layer

---
+++ ConvolutionalLayer.py
@@ -0,0 +1,66 @@
+import numpy as np
+import theano
+import theano.tensor as T
+from theano.tensor.nnet import conv
+from theano.tensor.signal import downsample
+def linear(z): return z
+def ReLU(z): return T.maximum(0.0, z)
+from theano.tensor.nnet import sigmoid
+from theano.tensor import tanh
+
+#   Defining the convolutional-pooling layer class.
+#   Class constructor:
+#   ConvPoolLayer(imageShape, filterShape, poolSize, activationFnc)
+#           imageShape      = (mini_batch_size, numberOfChannels, width, height)
+#           filterShape     = (numberOfFilters, numberOfInputs, filterWidth, filterHeight)
+#           pooSize         = (width, height)
+#           activationFnc   = ReLU, sigmoid, tanh, linear
+#
+#   Note: In the convolutional-pooling layers, the output size would be:
+#       output_size = (input_size - filter_size + 1) / pool_size
+
+
+class ConvPoolLayer(object):
+
+    def __init__(self, filter_shape, image_shape, poolsize=(2, 2),
+                 activation_fn=sigmoid):
+        self.filter_shape = filter_shape
+        self.image_shape = image_shape
+        self.poolsize = poolsize
+        self.activation_fn=activation_fn
+
+        # initialize weights and biases from a normal distribution
+
+        n_out = (filter_shape[0]*np.prod(filter_shape[2:])/np.prod(poolsize))
+        self.w = theano.shared(
+            np.asarray(
+                np.random.normal(loc=0, scale=np.sqrt(1.0/n_out), size=filter_shape),
+                dtype=theano.config.floatX),
+            borrow=True)
+        self.b = theano.shared(
+            np.asarray(
+                np.random.normal(loc=0, scale=1.0, size=(filter_shape[0],)),
+                dtype=theano.config.floatX),
+            borrow=True)
+        self.params = [self.w, self.b]
+
+    # Setting input to the ConvPoolLayer
+    def set_inpt(self, inpt, inpt_dropout, mini_batch_size):
+        self.inpt = inpt.reshape(self.image_shape)
+
+        # Convolving the input with the filters
+        conv_out = conv.conv2d(
+            input=self.inpt, filters=self.w, filter_shape=self.filter_shape,
+            image_shape=self.image_shape)
+
+        # Down sampling with respective pooling size
+        pooled_out = downsample.max_pool_2d(
+            input=conv_out, ds=self.poolsize, ignore_border=True)
+
+        # Calling the activation function with down sampled feature map as the input
+
+        self.output = self.activation_fn(
+            pooled_out + self.b.dimshuffle('x', 0, 'x', 'x'))
+
+        # no dropout in the convolutional layers
+        self.output_dropout = self.output
